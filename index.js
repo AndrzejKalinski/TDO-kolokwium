dc4d82e564b9e225d1d0680214e2118a70efa5ae
AndrzejKalinski <s26993@pjwstk.edu.pl>
2025-06-13 13:02:08 +0200

    poprawki

--- index.js
+++ index.js
@@ -5,9 +5,4 @@ app.get('/', (req, res) => {
     res.send('Hello CI/CD World!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
